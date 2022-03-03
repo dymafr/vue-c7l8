b70124bf7c18cd47c0d74fd32f720dd3d9244742
dymafr <erwan1309@hotmail.com>
2022-03-03 11:18:47 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <button @click="toggle = !toggle">Toggle</button>
-  <Blog
-    v-if="toggle"
-    v-for="title of titles"
-    :title="title"
-    :key="title"
-    class="parent"
-  />
+  <template v-if="toggle">
+    <Blog v-for="title of titles" :title="title" :key="title" class="parent"
+  /></template>
 </template>
 
 <script setup lang="ts">
