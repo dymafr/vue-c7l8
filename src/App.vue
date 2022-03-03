0443d784b681e154406523466df7be1bf9c0e897
dymafr <erwan1309@hotmail.com>
2022-03-03 11:17:51 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <h2>Form</h2>
-  <input ref="input" type="text" placeholder="Nom" />
-  <ul>
-    <li ref="fruitRef" v-for="fruit of arr" :key="fruit">{{ fruit }}</li>
-  </ul>
-  <Blog ref="blogRef" />
+  <button @click="toggle = !toggle">Toggle</button>
+  <Blog
+    v-if="toggle"
+    v-for="title of titles"
+    :title="title"
+    :key="title"
+    class="parent"
+  />
 </template>
 
 <script setup lang="ts">
 import { onMounted, reactive, ref } from 'vue';
 import Blog from './Blog.vue';
 
-const input = ref<HTMLInputElement | null>(null);
-const arr = reactive(['pomme', 'fraise', 'poire']);
-const fruitRef = ref<HTMLLinkElement[] | []>([]);
-const blogRef = ref<InstanceType<typeof Blog> | null>(null);
+const titles = ref(['Un titre', 'Un autre titre']);
 
-onMounted(() => {
-  console.log(input.value?.focus);
-  console.log(fruitRef.value);
-  console.log(blogRef.value?.count);
-  console.log(blogRef.value?.arrTest);
-  blogRef.value?.display();
-});
+const toggle = ref(true);
 </script>
 
 <style scoped lang="scss"></style>
--- src/Blog.vue
+++ src/Blog.vue
@@ -1,15 +1,9 @@
 <template>
-  <h2>Blog</h2>
+  <h2>{{ title }}</h2>
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref, type Ref } from 'vue';
+import { ref } from 'vue';
 
-const count = ref(15);
-const arrTest = reactive([1,2,3]);
-function display() {
-    console.log('Display')
-}
-
-defineExpose<{count: Ref<number>, arrTest: number[], display: () => void}>({count, arrTest, display})
+defineProps<{ title: string }>();
 </script>
